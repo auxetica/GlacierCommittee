fa734509de7f088c3478b6bf6430c7892861b316
mohammadr09 <mohammadnyc96@gmail.com>
2025-12-06 17:27:01 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/commands/DriveTurn.java
@@ -0,0 +1,42 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
+
+public class DriveTurn extends Command {
+    private final Drivetrain drivetrain;
+    private final double speed, deg, inches;
+    private double targetHeading;
+
+    public DriveTurn(double speed, double deg, double inches, Drivetrain drivetrain) {
+        this.speed = speed;
+        this.deg = deg;
+        this.drivetrain = drivetrain;
+        this.inches = inches;
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void initialize() {
+        targetHeading = drivetrain.getGyroAngleY() + deg;
+        drivetrain.resetEncoders();
+    }
+
+    @Override
+    public void execute() {
+        double currentHeading = drivetrain.getGyroAngleY();
+        double headingError = targetHeading - currentHeading;
+        double turnSpeed = headingError * 0.01; // Proportional control for turning
+        drivetrain.arcadeDrive(speed, turnSpeed);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return drivetrain.getAverageDistanceInch() >= inches;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.arcadeDrive(0, 0);
+    }
+}
